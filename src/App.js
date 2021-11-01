d77586000cd356b61bec01ca978dbaf67577b72a
Davey Struijk <mail@daveystruijk.com>
2021-11-01 09:59:41 +0100

    Add basic tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { useChromeStorageLocal } from 'use-chrome-storage';
-import ContentEditable from 'react-contenteditable';
 import { Task } from './Task';
 
-const INITIAL_TASKS = [
-  {
+const INITIAL_TASKS = {
+  1: {
     id: 1,
     category: 'personal',
+    text: 'Hi',
   },
-];
+};
 
 export function App() {
   const [tasks, setTasks] = useChromeStorageLocal('tasks', INITIAL_TASKS);
 
+  const resetTasks = () => {
+    setTasks(INITIAL_TASKS);
+  };
+
+  const setTask = (task) => setTasks({ ...tasks, [`${task.id}`]: task });
+
   return (
-    <div className="tasks">
-      {tasks.map((task) => (
-        <Task key={task.id} task={task} />
-      ))}
+    <div>
+      <div className="tasks">
+        {Object.keys(tasks).map((key) => (
+          <Task key={key} task={tasks[`${key}`]} setTask={setTask} />
+        ))}
+      </div>
+      <div className="debug">
+        <button onClick={resetTasks}>Reset</button>
+      </div>
     </div>
   );
 }
--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import EdiText from 'react-editext';
 
 export function Task(props) {
+  const { task, setTask } = props;
 
+  const onSave = (text) => setTask({ ...task, text });
 
   return (
-    <div className="task">
-      <p>Task</p>
+    <div className={`task category-${task.category}`}>
+      <div className="icon" />
+      <EdiText
+        value={task.text}
+        onSave={onSave}
+        submitOnEnter={true}
+        cancelOnEscape={true}
+        editOnViewClick={true}
+        showButtonsOnHover={true}
+      ></EdiText>
     </div>
   );
 }
