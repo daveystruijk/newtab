f3a1709b42b39d7f2bf2cbc71bb237d28616e55c
Davey Struijk <mail@daveystruijk.com>
2022-02-17 16:54:04 +0100

    Click on blank page before showing tasks

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import { groupBy, range } from 'lodash';
 import { DAY_FORMAT } from '../constants';
@@ -6,10 +6,13 @@ import { useTaskProvider } from '../providers/TaskProvider';
 import { Day } from './Day';
 
 export const MainPage = () => {
+  const [visible, setVisible] = useState(false);
+
   const { tasks } = useTaskProvider();
   const hasTasks = tasks.length > 0;
 
   useEffect(() => {
+    if (!hasTasks || !visible) { return; }
     const el = document.getElementsByClassName('today')[0];
     el.scrollIntoView();
   }, [hasTasks]);
@@ -21,28 +24,32 @@ export const MainPage = () => {
     navigator.clipboard.writeText(JSON.stringify(tasks));
 
   const days = range(-300, 90)
-    .map(i => now.add(i, 'days'))
-    .map(date => date.format(DAY_FORMAT));
+    .map((i) => now.add(i, 'days'))
+    .map((date) => date.format(DAY_FORMAT));
 
   const tasksPerDay = groupBy(tasks, 'day');
+
+  if (!visible) {
+    return (
+      <div
+        style={{ width: '100%', height: '100%' }}
+        onClick={() => setVisible(true)}
+      ></div>
+    );
+  }
 
   return (
     <div>
       <div className="tasks">
-        {days.map(day => {
-          const visible = (day === today) || (day in tasksPerDay);
-          if (!visible) { return; }
+        {days.map((day) => {
+          const visible = day === today || day in tasksPerDay;
+          if (!visible) {
+            return;
+          }
 
           const date = dayjs(day, DAY_FORMAT);
           const tasks = tasksPerDay[day] || [];
-          return (
-            <Day
-              key={day}
-              day={day}
-              date={date}
-              tasks={tasks}
-            />
-          );
+          return <Day key={day} day={day} date={date} tasks={tasks} />;
         })}
       </div>
       <div className="debug">
@@ -50,5 +57,4 @@ export const MainPage = () => {
       </div>
     </div>
   );
-}
-
+};
